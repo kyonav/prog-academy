449ef1756341cda501840318a454e023a79e8a98
kyonav <tokyonav@proton.me>
2025-03-12 15:05:44 -0300

    final version?

--- little_projects/calc/Program.cs
+++ little_projects/calc/Program.cs
@@ -4,29 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
+
         string[] operHistory = new string[100];
 
         int histCounter = 0;
         while (true){
-            Console.WriteLine("Limited calc");
+            Console.WriteLine(">>> Low budget calculator <<<");
             Console.WriteLine();
 
             Console.WriteLine("1 - Sum");
             Console.WriteLine("2 - Subtract");
             Console.WriteLine("3 - Multiply");
             Console.WriteLine("4 - Divide");
+            Console.WriteLine();
+
             Console.WriteLine("5 - Mult table");
             Console.WriteLine("6 - History");
             Console.WriteLine();
             Console.WriteLine("Q - Quit");
             Console.WriteLine();
 
+            Console.Write("> ");
             string userInput = Console.ReadLine().ToUpper();
+
+            Console.Clear();
 
             if (userInput == "5"){
                 Console.Clear();
 
-                Console.WriteLine("Multiplication table");
+                Console.WriteLine(" >>> Multiplication table <<<");
                 Console.WriteLine();
 
                 Console.Write("Type a number > ");
@@ -34,30 +41,68 @@ class Program
                 int num = 0;
                 num = Int32.Parse(Console.ReadLine());
 
+                Console.WriteLine();
+
                 for (int counter = 1; counter <= 10; counter++){
                     int tabResult = num * counter;
                     Console.WriteLine($"{num} x {counter} = {tabResult}");
 
                 } 
 
+                Console.WriteLine();
 
-               Console.ReadLine();
-               continue;
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+
+                Console.Clear();
+
+                continue;
+
             }
 
             if (userInput == "6"){
-                foreach (string item in operHistory){
-                    if (item != null){
-                        Console.WriteLine(item);
+                if (operHistory[0] != null){
+                    Console.Clear();
 
+                    Console.WriteLine(">>> Operation history <<<");
+                    Console.WriteLine();
+
+                    foreach (string item in operHistory){
+                        if (item != null){
+                            Console.WriteLine(item);
+
+                        }
                     }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+
+                    Console.Clear();
+
+                    continue;
+
+                } else {
+                    Console.Clear();
+
+                    Console.WriteLine("The history is empty.");
+                    Console.WriteLine();
+
+                    continue;
+
                 }
             }
 
-            Console.Write("Type the 1st number: ");
+            if (userInput == "Q"){
+                Console.WriteLine("Quitting...");
+                break;
+
+            }
+
+            Console.Write("Type the 1st number > ");
             float firstNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Type the 2nd number: ");
+            Console.Write("Type the 2nd number > ");
             float secondNumber = Convert.ToInt32(Console.ReadLine());
 
             if (secondNumber == 0 && userInput == "4"){
@@ -83,20 +128,31 @@ class Program
                 result = firstNumber / secondNumber;
                 operHistory[histCounter] = $"{firstNumber} / {secondNumber} = {result}";
 
+            } else {
+                Console.Clear();
+                
+                Console.WriteLine("Invalid option. Try again.");
+                continue;
+
             }
 
             histCounter++;
 
             Console.WriteLine();
-            Console.WriteLine("Result > ");
+            Console.Write("Result > ");
             Console.WriteLine(result.ToString("F2"));
             Console.WriteLine();
 
             Console.Write("Continue? Y/N > ");
             if (Console.ReadLine() != "Y"){
+                Console.Clear();
+
+                Console.WriteLine("Quitting...");
                 break;
 
             }
+
+            Console.Clear();
         }
     }
 }
--- little_projects/calc/README.md
+++ little_projects/calc/README.md
@@ -1,5 +1,7 @@
 # Low budget Calculator
 
+![](./misc/app_running.gif)
+
 ## Introduction
 
 A powerful console calculator that can do 4 mathematic operations.
@@ -7,6 +9,8 @@ A powerful console calculator that can do 4 mathematic operations.
 ## Functionalities
 
 **Basic operations**: Can do sum, sub, mult and div.
+
+**Multiplication table**: Can perform a mult table with a number provided by the user.
 
 ## How to use
 
@@ -17,3 +21,29 @@ A powerful console calculator that can do 4 mathematic operations.
 ```
 dotnet restore
 ```
+
+4. Compile the project using the command:
+
+```
+dotnet build --configuration release_name
+```
+
+5. Execute the project compiling in real time.
+
+```
+dotnet run --project ???to_do???
+```
+
+6. To run the compiled executable, navigate to `./prog-academy/little-projects/calc/bin/release_name/net-9.0/little-projects` and run:
+
+Windows
+```
+./little-projects.exe
+```
+
+Linux
+```
+./little-projects
+```
+
+TODO: Fix the namespace of the project.
