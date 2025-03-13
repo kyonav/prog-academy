1fc8adb83a606e89956529e850b8f66262f5728a
kyonav <tokyonav@proton.me>
2025-03-13 16:24:33 -0300

    add showMenu() method

--- little_projects/calc/Program.cs
+++ little_projects/calc/Program.cs
@@ -9,6 +9,132 @@ class Program
         string[] operHistory = new string[100];
 
         int histCounter = 0;
+        // prints the menu, then redirects to the selected option
+        string userInput = showMenu();
+
+        Console.Clear();
+
+        //prints the mult table of a choosen number
+        if (userInput == "5"){
+            Console.Clear();
+
+            Console.WriteLine(" >>> Multiplication table <<<");
+            Console.WriteLine();
+
+            Console.Write("Type a number > ");
+
+            int num = 0;
+            num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int counter = 1; counter <= 10; counter++){
+                int tabResult = num * counter;
+                Console.WriteLine($"{num} x {counter} = {tabResult}");
+
+            } 
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+            Console.Clear();
+
+        }
+
+        // prints the history operation
+        if (userInput == "6"){
+            if (operHistory[0] != null){
+                Console.Clear();
+
+                Console.WriteLine(">>> Operation history <<<");
+                Console.WriteLine();
+
+                foreach (string item in operHistory){
+                    if (item != null){
+                        Console.WriteLine(item);
+
+                    }
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+
+                Console.Clear();
+
+
+            } else {
+                Console.Clear();
+
+                Console.WriteLine("The history is empty.");
+                Console.WriteLine();
+
+
+            }
+        }
+
+        // checks if the user wants to quit
+        if (userInput == "Q"){
+            Console.WriteLine("Quitting...");
+
+        }
+
+        Console.Write("Type the 1st number > ");
+        float firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Type the 2nd number > ");
+        float secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (secondNumber == 0 && userInput == "4"){
+            Console.WriteLine("Cannot divide by zero.");
+
+        }
+
+        float result = 0;
+        if (userInput == "1"){
+            result = firstNumber + secondNumber;
+            operHistory[histCounter] = $"{firstNumber} + {secondNumber} = {result}";
+
+        } else if (userInput == "2"){
+            result = firstNumber - secondNumber;
+            operHistory[histCounter] = $"{firstNumber} - {secondNumber} = {result}";
+
+        } else if (userInput == "3"){
+            result = firstNumber * secondNumber;
+            operHistory[histCounter] = $"{firstNumber} * {secondNumber} = {result}";
+
+        } else if (userInput == "4"){
+            result = firstNumber / secondNumber;
+            operHistory[histCounter] = $"{firstNumber} / {secondNumber} = {result}";
+
+        } else {
+            Console.Clear();
+
+            Console.WriteLine("Invalid option. Try again.");
+
+        }
+
+        histCounter++;
+
+        Console.WriteLine();
+        Console.Write("Result > ");
+        Console.WriteLine(result.ToString("F2"));
+        Console.WriteLine();
+
+        Console.Write("Continue? Y/N > ");
+        if (Console.ReadLine() != "Y"){
+            Console.Clear();
+
+            Console.WriteLine("Quitting...");
+
+        }
+
+        Console.Clear();
+    }
+
+    static string showMenu(){
         while (true){
             Console.WriteLine(">>> Low budget calculator <<<");
             Console.WriteLine();
@@ -26,133 +152,9 @@ class Program
             Console.WriteLine();
 
             Console.Write("> ");
-            string userInput = Console.ReadLine().ToUpper();
+            string userInput = Console.ReadLine()!.ToUpper();
+            return userInput;
 
-            Console.Clear();
-
-            if (userInput == "5"){
-                Console.Clear();
-
-                Console.WriteLine(" >>> Multiplication table <<<");
-                Console.WriteLine();
-
-                Console.Write("Type a number > ");
-
-                int num = 0;
-                num = Int32.Parse(Console.ReadLine());
-
-                Console.WriteLine();
-
-                for (int counter = 1; counter <= 10; counter++){
-                    int tabResult = num * counter;
-                    Console.WriteLine($"{num} x {counter} = {tabResult}");
-
-                } 
-
-                Console.WriteLine();
-
-                Console.WriteLine("Press enter to continue...");
-                Console.ReadLine();
-
-                Console.Clear();
-
-                continue;
-
-            }
-
-            if (userInput == "6"){
-                if (operHistory[0] != null){
-                    Console.Clear();
-
-                    Console.WriteLine(">>> Operation history <<<");
-                    Console.WriteLine();
-
-                    foreach (string item in operHistory){
-                        if (item != null){
-                            Console.WriteLine(item);
-
-                        }
-                    }
-                    Console.WriteLine();
-
-                    Console.WriteLine("Press enter to continue...");
-                    Console.ReadLine();
-
-                    Console.Clear();
-
-                    continue;
-
-                } else {
-                    Console.Clear();
-
-                    Console.WriteLine("The history is empty.");
-                    Console.WriteLine();
-
-                    continue;
-
-                }
-            }
-
-            if (userInput == "Q"){
-                Console.WriteLine("Quitting...");
-                break;
-
-            }
-
-            Console.Write("Type the 1st number > ");
-            float firstNumber = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Type the 2nd number > ");
-            float secondNumber = Convert.ToInt32(Console.ReadLine());
-
-            if (secondNumber == 0 && userInput == "4"){
-                Console.WriteLine("Cannot divide by zero.");
-                continue;
-
-            }
-
-            float result = 0;
-            if (userInput == "1"){
-                result = firstNumber + secondNumber;
-                operHistory[histCounter] = $"{firstNumber} + {secondNumber} = {result}";
-
-            } else if (userInput == "2"){
-                result = firstNumber - secondNumber;
-                operHistory[histCounter] = $"{firstNumber} - {secondNumber} = {result}";
-
-            } else if (userInput == "3"){
-                result = firstNumber * secondNumber;
-                operHistory[histCounter] = $"{firstNumber} * {secondNumber} = {result}";
-
-            } else if (userInput == "4"){
-                result = firstNumber / secondNumber;
-                operHistory[histCounter] = $"{firstNumber} / {secondNumber} = {result}";
-
-            } else {
-                Console.Clear();
-                
-                Console.WriteLine("Invalid option. Try again.");
-                continue;
-
-            }
-
-            histCounter++;
-
-            Console.WriteLine();
-            Console.Write("Result > ");
-            Console.WriteLine(result.ToString("F2"));
-            Console.WriteLine();
-
-            Console.Write("Continue? Y/N > ");
-            if (Console.ReadLine() != "Y"){
-                Console.Clear();
-
-                Console.WriteLine("Quitting...");
-                break;
-
-            }
-
-            Console.Clear();
         }
     }
 }
