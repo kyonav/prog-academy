bbffe9dd16e1a7e36b5cb8e8c0fbc0f17cbdb613
kyonav <tokyonav@proton.me>
2025-03-19 23:47:56 -0300

    final commit

--- knowledge_test/triangles_classifier/Program.cs
+++ knowledge_test/triangles_classifier/Program.cs
@@ -1,221 +1,47 @@
-﻿namespace knowledge_test;
+﻿using System.Reflection.Metadata.Ecma335;
 
-class Program
+namespace triangle_classifier;
+
+internal class Program
 {
-    static void Main(string[] args)
+    public static void Main() // surely this can be public, right?
     {
+        TriangleClassifier.AppMenuMain();
 
-        static void app_menu_help(){
-            Console.Clear();
-            
-            Console.Write("Loading help...");
-            Thread.Sleep(1500);
+        string userInput = Console.ReadLine();
+        if (Int32.TryParse(userInput, out _))
+        {
 
-            Console.Clear();
+            if (userInput == "1")
+            {
+                    TriangleClassifier.AppRunStart();
+                    int[] valuesNumArray = TriangleClassifier.AppRunUserInput();
+                    TriangleClassifier.AppRunTriangleVerification(valuesNumArray);
+                    TriangleClassifier.AppMenuEnd();
 
-            do {
-                Console.Write("In order for this program to work ");
-                Console.Write("correctly, you need to follow the existance ");
-                Console.Write("condition. Ex:");
-                Console.WriteLine();
-                Console.WriteLine();
 
-                Console.WriteLine("X + Y > Z");
-                Console.WriteLine("Y + Z > X");
-                Console.WriteLine("X + Z > Y");
-                Console.WriteLine();
+            }
+            else if (userInput == "2")
+            {
+                TriangleClassifier.AppMenuHelp();
+            }
+            else if (userInput == "0")
+            {
+                Console.Clear();
 
-                Console.WriteLine("1 - Main menu");
-                Console.WriteLine();
-
-                Console.Write("> ");
-
-                if (Int32.TryParse(Console.ReadLine(), out int userInput)){
-                    if (userInput == 1){
-                        app_menu_main();
-
-                    } else {
-                        Console.Clear();
-                        Console.WriteLine("Invalid option. Try again...");
-                        Console.WriteLine();
-
-                    }
-
-                } else {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input. Type only numbers...");
-                    Console.WriteLine();
-
-                }
-
-            } while (true);
+                Console.WriteLine("Closing app...");
+                Environment.Exit(0);
+            }
+            else
+            {
+                TriangleClassifier.InvalidOptionLog();
+                Main();
+            }
         }
-
-
-        static void app_menu_main(){
-            Console.Clear();
-
-            Console.WriteLine("Loading menu...");
-            Thread.Sleep(1500);
-
-            Console.Clear();
-
-            do {
-                Console.WriteLine(">>> Triangle classifier <<<");
-                Console.WriteLine();
-
-                Console.WriteLine("1 - Start");
-                Console.WriteLine("2 - Help");
-                Console.WriteLine();
-                Console.WriteLine("0 - Quit");
-                Console.WriteLine();
-                
-                Console.Write("> ");
-
-                if (Int32.TryParse(Console.ReadLine(), out int userInput)){
-                    if (userInput == 1){
-                        app_start_def();
-
-                    } else if (userInput == 2){
-                        app_menu_help(); 
-
-                    } else if (userInput == 0){
-                        Console.Clear();
-
-                        Console.WriteLine("Quitting...");
-                        Thread.Sleep(1500);
-
-                        break;
-
-                    } else {
-                        Console.Clear();
-
-                        Console.WriteLine("Invalid option. Try again...");
-                        Console.WriteLine();
-                    }
-
-                } else {
-                    Console.Clear();
-
-                    Console.WriteLine("Invalid input. Type only numbers...");
-                    Console.WriteLine();
-
-                }
-
-            } while (true);
+        else
+        {
+            TriangleClassifier.InvalidInputLog();
+            Main();
         }
-
-        static void app_menu_end(){
-            do {
-                Console.WriteLine();
-
-                Console.WriteLine("1 - Try again");
-                Console.WriteLine("2 - Main menu");
-                Console.WriteLine();
-
-                Console.Write("> ");
-
-                if (Int32.TryParse(Console.ReadLine(), out int userInput)){
-                    if (userInput == 1){
-                        Console.Clear();
-
-                        app_start_def();
-
-                    } else if (userInput == 2){
-                        Console.Clear();
-
-                        app_menu_main();
-
-                    } else {
-                        Console.Clear();
-
-                        Console.WriteLine("Invalid option. Try again...");
-
-                    }
-
-                } else {
-                    Console.Clear();
-
-                    Console.WriteLine("Invalid input. Type only numbers...");
-
-                }
-
-            } while (true);
-        }
-
-        static void app_start_def(){
-            Console.Clear();
-
-            Console.WriteLine("Loading app...");
-            Thread.Sleep(1500);
-
-            Console.Clear();
-
-            string[] valuesChar = new string[3] {"X", "Y", "Z"};        
-            int[] valuesNum = new int[3];
-            
-            do {
-                for (int index = 0; index < valuesNum.Length;){
-
-                    Console.WriteLine($"Input the {valuesChar[index]} value.");
-                    Console.Write("> ");
-
-                    if (Int32.TryParse(Console.ReadLine(), out valuesNum[index])){
-                        index++;
-
-                    } else {
-                        Console.Clear();
-
-                        Console.WriteLine("Invalid input. Type only numbers.");
-                        Console.WriteLine();
-                    }
-                }
-
-                //foreach (int item in valuesNum){
-                //    Console.WriteLine(item);
-                //}
-
-                if ((valuesNum[0] + valuesNum[1]) > valuesNum[2] &&
-                     (valuesNum[1] + valuesNum[2]) > valuesNum[0] &&
-                     (valuesNum[0] + valuesNum[2]) > valuesNum[1]){
-
-                    string userOutput;
-                    if (valuesNum[0] == valuesNum[1] && valuesNum[0] == valuesNum[2]){
-                        userOutput = "equilateral"; 
-
-                    } else if (valuesNum[0] == valuesNum[1]) {
-                        userOutput = "isosceles";
-
-                    } else if (valuesNum[1] == valuesNum[2]){
-                        userOutput = "isosceles";
-
-                    } else if (valuesNum[0] == valuesNum[2]){
-                        userOutput = "isosceles";
-
-                    } else {
-                        userOutput = "scalene";
-
-                    }
-
-                    Console.Clear();
-
-                    Console.WriteLine($"Your triangle is > {userOutput} <");
-
-                } else {
-                    Console.Clear();
-
-                    Console.WriteLine("Invalid triangle. Try again...");
-                    Console.WriteLine();
-
-                    continue;
-
-                }
-
-                app_menu_end();
-
-            } while (true);
-        }
-        
-        app_menu_main();
     }
 }
---
+++ knowledge_test/triangles_classifier/README.md
@@ -0,0 +1,56 @@
+# Low budget Hangman Game!
+
+![](https://i.imgur.com/rFn1RA1.gif)
+
+## Introduction
+
+An app to classify triangles.
+
+## Project
+
+Developed during the Fullstack course at [Academia do Programador](https://www.academiadoprogramador.net) 2025
+
+## Features
+
+- **Input Exception**: Allow the user to only enter valid values.
+- **Instant feedback**
+
+## Requirements
+
+- .NET SDK (recommended .NET 8.0 or higher) for compiling and running the project.
+
+## Compiling and running
+
+1. Clone the repo and download the source code.
+2. Open the terminal and navigate to the project folder.
+3. Use the command below to restore the project dependencies.
+
+```
+dotnet restore triangle_classifier.csproj
+```
+
+4. Compile the project using the command:
+
+```
+dotnet build triangle_classifier.csproj --configuration Release
+```
+
+5. Execute the project compiling in real time.
+
+```
+dotnet run --project triangle_classifier.csproj
+```
+
+6. To run the compiled executable; cd into `./prog-academy/knowledge_test/triangle_classifier/bin/Release/net-9.0/` on your terminal and type:
+
+> Windows
+```
+./little-projects.exe
+```
+
+> Linux
+```
+./little-projects
+```
+
+*Or just run graphically by double clicking it on your file manager.*
