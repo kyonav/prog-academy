4e42897de7082ccc97e9ccdf6e12efe8fe05059f
kyonav <tokyonav@proton.me>
2025-03-23 21:51:47 -0300

    ex3 done

---
+++ little_projects/Ex_List/Ex3.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inital mileage > ");
+            double initial = double.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.Write("Final mileage > ");
+            double final = double.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.Write("Fuel consumed > ");
+            double fConsumed = double.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            double mileageTraveled = (final - initial);
+
+            double consumptionPerKm = (mileageTraveled / fConsumed);
+
+            Console.WriteLine($"The fuel consumption during the route was ~ {consumptionPerKm} km/L");
+        }
+    }
+}
