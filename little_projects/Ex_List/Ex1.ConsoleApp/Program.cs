88207b515702c6ad6d8113f701515c2ae74398cf
kyonav <tokyonav@proton.me>
2025-03-23 21:31:19 -0300

    ex1 && ex2 done

---
+++ little_projects/Ex_List/Ex1.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex1.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the length > ");
+            int length = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.Write("Type the height > ");
+            int width = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.Write("Type the width > ");
+            int height = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.WriteLine($"The volume is {length * width * height}");
+        }
+    }
+}
---
+++ little_projects/Ex_List/Ex2.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the radius > ");
+            double radius = double.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            Console.Write("Type the height > ");
+            double height = double.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            double result = Math.PI * Math.Pow(radius, 2) * height;
+
+            Console.WriteLine($"The cilinder volume is > {result}");
+        }
+    }
+}
