4a0a79479a1cbb9d302b74a33e870a33231d9c6c
kyonav <tokyonav@proton.me>
2025-03-24 06:03:22 -0300

    ex8 not done

--- little_projects/Ex_List/Ex2.ConsoleApp/obj/Debug/net8.0/Ex2.ConsoleApp.AssemblyInfo.cs
+++ little_projects/Ex_List/Ex2.ConsoleApp/obj/Debug/net8.0/Ex2.ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex2.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfba8b427cd3b74471386e85dc344f0da7d149b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451e7d5d83e731fea1668ab5afa5eba379e1d2be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex2.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex2.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ little_projects/Ex_List/Ex8.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Ex8.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+        }
+    }
+}
