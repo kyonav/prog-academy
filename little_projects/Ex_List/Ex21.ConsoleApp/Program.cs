b081c9ba5c1a15430b53f83e8a03953c818c16cc
kyonav <tokyonav@proton.me>
2025-03-24 09:35:22 -0300

    ex21 done

---
+++ little_projects/Ex_List/Ex21.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Ex21.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number > ");
+            int number = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            int updatedNumber = number;
+
+            for (int n = number; n > 1; n--)
+            {
+                updatedNumber = updatedNumber * (n - 1);
+
+                Console.WriteLine($"{n} x {n -1} = {updatedNumber}");
+            }
+            
+
+        }
+    }
+}
