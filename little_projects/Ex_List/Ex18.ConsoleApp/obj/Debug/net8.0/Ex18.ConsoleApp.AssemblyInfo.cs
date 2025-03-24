756c617913917cc1eede99765c677638229d8c77
kyonav <tokyonav@proton.me>
2025-03-24 08:59:46 -0300

    ex20 done

--- little_projects/Ex_List/Ex18.ConsoleApp/obj/Debug/net8.0/Ex18.ConsoleApp.AssemblyInfo.cs
+++ little_projects/Ex_List/Ex18.ConsoleApp/obj/Debug/net8.0/Ex18.ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex18.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bc2cc9eaf5d31ea21d5ecfeb897b939c686b7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d91e2320615732d1c7555def19685d342bde2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex18.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex18.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ little_projects/Ex_List/Ex20.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex20.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number > ");
+            int n = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine();
+
+            for (int t = 1; t <= 10; t++)
+            {
+                Console.WriteLine($"{n} x {t} = {n * t}");
+            }
+        }
+    }
+}
